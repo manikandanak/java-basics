5449bddb5e047a5dd12c65678c8a640de4e1cf02
manikandanak <manikandanak.it@gmail.com>
2017-07-20 10:50:22 +0530

    Add files via upload

---
+++ Customer_Main.java
@@ -0,0 +1,10 @@
+package customer_details;
+
+public class Customer_Main {
+
+	public static void main(String[] args) {
+		Customer_Operation customer = new Customer_Operation();
+		//new Customer_Operation().getUserInput();
+		customer.getUserInput();		
+	}
+}

770e023cf5cf2f1bf3f9211b58159e2e0512e231
manikandanak <manikandanak.it@gmail.com>
2017-07-20 10:52:35 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,10 @@
-package employee;
-
-import java.util.Scanner;
+package test;
 
 public class Main {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		Scanner sc=new Scanner(System.in);
-		String n="";
-		do{
-			try{
-		StoredEmp emp = new StoredEmp();
-		emp.getDetails();
-		System.out.println("do u wanna cont type 1 for cont");
-		 n=sc.next();
-			}
-			catch(Exception e){
-				e.printStackTrace();
-				
-			}
-		}
-		while(n=="y");
+		
+		 EmployeeOperation employee=new EmployeeOperation();
+		 employee.getUserInput();
 	}
-
 }
